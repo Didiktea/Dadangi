399331b916455f9db700da995178005ad7a315e8
Didiktea <85869003+Didiktea@users.noreply.github.com>
2021-06-15 17:53:25 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -29,9 +29,9 @@ I'm group manager bot and Anti-spam for RR Players🃏
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
-Made from the source code of [Aditya 🇮🇳](t.me/xditya)
+Made from the source code of [Didiktea](t.me/Didiktea)
 
-Special thanks to [this person](t.me/denver02) for helping me.
+Special thanks to [this person](t.me/rsrmusic) for helping me.
 
 Deploy a bot like me [now](www.github.com/xditya/GroupManager)
 
@@ -39,7 +39,7 @@ Watch [video tutorial](https://www.youtube.com/watch?v=gXXFpTAk6Vo&feature=youtu
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/Dadangi_bot?startgroup=true).
 """
 
 
