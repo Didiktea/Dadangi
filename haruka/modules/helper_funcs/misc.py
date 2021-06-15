cbb1e8144356b4bf0dd30f30c98846cba912b067
Didiktea <85869003+Didiktea@users.noreply.github.com>
2021-06-15 23:01:23 +0530

    Update misc.py

--- haruka/modules/helper_funcs/misc.py
+++ haruka/modules/helper_funcs/misc.py
@@ -53,17 +53,17 @@ def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None)
                                     callback_data="{}_module({},{})".format(prefix, chat, x.__mod_name__.lower())) for x
              in module_dict.values()])
 
-    pairs = list(zip(modules[::1], modules[1::1]))
+    pairs = list(zip(modules[::2], modules[2::1]))
 
-    if len(modules) % 1 == 1:
+    if len(modules) % 2 == 1:
         pairs.append((modules[-1],))
 
-    max_num_pages = ceil(len(pairs) / 10)
+    max_num_pages = ceil(len(pairs) / 20)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 10:
-        pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
+    if len(pairs) > 20:
+        pairs = pairs[modulo_page * 20:20 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
              EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
              EqInlineKeyboardButton(">>", callback_data="{}_next({})".format(prefix, modulo_page)))]
