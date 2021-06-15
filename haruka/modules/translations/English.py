7afa00ca58d46b9e8794e69ee5d11fe18bb08428
Didiktea <85869003+Didiktea@users.noreply.github.com>
2021-06-16 00:08:45 +0530

    Update English.py

--- haruka/modules/translations/English.py
+++ haruka/modules/translations/English.py
@@ -213,19 +213,13 @@ If you enjoying using me and/or would like to help me survive in the wild, hit /
 """,
 
     "send-help": """Hey there! My name is *{}*.
-I'm a modular group management bot with a few fun extras! Have a look at the following for an idea of some of the things I can help you with.
+I'm a modular Mizo Bot with a few fun extras!
 
-Main commands available:
- - /start: Cool command to check if the bot is alive or not
- - /help: PM's you this message.
- - /help <module name>: PM's you info about that module.
- - /donate: information about how to donate!
- - /lang: Change bot language
- - /settings:
-   - in PM: will send you your settings for all supported modules.
-   - in a group: will redirect you to pm, with all that chat's settings.
-   {}
-   """,
+*『HELP BUTTONS HERE』 *
+☞︎︎︎ _A hnuai a button ho khu ka thil tih theih te chu an ni e._ 
+☞︎︎︎ _Han hmet chhin la a command dan tur te thleng in a inziak vek e._ 
+   *❍ Kan Sakruang Mizoram Tan❍*
+""",
 
     "send-group-settings": """Hi there! There are quite a few settings for *{}* - go ahead and pick what
 you're interested in.""",
