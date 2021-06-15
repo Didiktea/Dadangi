387b99b4bdd6bb719291ff17b9a1d449a9304bfb
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2021-06-15 21:54:52 +0530

    Update misc.py

--- haruka/modules/helper_funcs/misc.py
+++ haruka/modules/helper_funcs/misc.py
@@ -64,9 +64,7 @@ def paginate_modules(chat_id, page_n: int, module_dict: Dict, prefix, chat=None)
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-             EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start"),
-             EqInlineKeyboardButton(">>", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]
     else:
         pairs += [[EqInlineKeyboardButton("⬅️ Back", callback_data="bot_start")]]
 
